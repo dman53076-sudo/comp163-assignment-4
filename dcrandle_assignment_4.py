15b97b6efa593f9b9c6061223c86e39ad1836371
dman53076-sudo <dman53076@gmail.com>
2025-09-26 19:19:13 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ dcrandle_assignment_4.py
@@ -0,0 +1,12 @@
+#Student starting stats
+student_name = "Daniel Crandle"
+current_gpa = 3.5
+study_hours = 12
+social_points = 75
+stress_level = 40
+#Welcome message and stats display
+print(f"Welcome, {student_name}!")
+print(f"Your current GPA is: {current_gpa}")
+print(f"Your study hours this week: {study_hours}")
+print(f"Your social points: {social_points}")
+print(f"Your stress level: {stress_level}")
